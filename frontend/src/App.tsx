fe0987aac4d66dbe9ab505a40ea15df4a7d1fd78
MBWankhade <mwankhade718@gmail.com>
2026-05-17 17:10:50 +0530

    Initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+type AppProps = {}
+
+export default function App(_props: AppProps) {
+  return (
+    <div className="app-root">
+      <header>
+        <h1>GetPotentialLead</h1>
+      </header>
+      <main>
+        <p>This is the frontend shell. Replace with your app components.</p>
+      </main>
+    </div>
+  )
+}
